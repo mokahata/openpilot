d3ffb0f5844e11870ddabbe3cdef1a43cadf5005
Bugsy <30946888+bugsy924@users.noreply.github.com>
2020-03-30 12:39:32 +1100

    force attempt 2

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -150,7 +150,7 @@ def get_car(logcan, sendcan, has_relay=False):
 
   if candidate is None:
     cloudlog.warning("car doesn't match any fingerprints: %r", fingerprints)
-    candidate = "mock"
+    candidate = "CHEVROLET SUBURBAN PREMIER 2019"
 
   CarInterface, CarController = interfaces[candidate]
   car_params = CarInterface.get_params(candidate, fingerprints, has_relay, car_fw)
